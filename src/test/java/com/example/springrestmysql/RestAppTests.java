b7cfab3b5b5f35a0492ab63f7aa21e6434831539
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-04-03 09:52:55 +0200

    initial commit

---
+++ src/test/java/com/example/springrestmysql/RestAppTests.java
@@ -0,0 +1,16 @@
+package com.example.springrestmysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
